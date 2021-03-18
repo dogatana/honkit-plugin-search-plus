6c1fb99358c246d62d58982965e808f36a7be52f
Toshihiko Ichida <dogatana@gmail.com>
2021-03-18 15:20:11 +0900

    update index.js
    - format code
    - repalce var to const or let
    - preserve the charcters of a page text as much as possible

--- index.js
+++ index.js
@@ -1,64 +1,74 @@
-var Entities = require('html-entities').AllHtmlEntities
+const Entities = require('html-entities').AllHtmlEntities;
 
-var Html = new Entities()
+const Html = new Entities();
 
 // Map of Lunr ref to document
-var documentsStore = { }
+const documentsStore = {};
 
 module.exports = {
-  book: {
-    assets: './assets',
-    js: [
-      'jquery.mark.min.js',
-      'search.js'
-    ],
-    css: [
-      'search.css'
-    ]
-  },
-
-  hooks: {
-    // Index each page
-    'page': function (page) {
-      if (this.output.name !== 'website' || page.search === false) {
-        return page
-      }
-
-      var text
-
-      this.log.debug.ln('index page', page.path)
-
-      text = page.content
-      // Decode HTML
-      text = Html.decode(text)
-      // Strip HTML tags
-      text = text.replace(/(<([^>]+)>)/ig, '')
-      text = text.replace(/[\n ]+/g, ' ')
-      var keywords = []
-      if (page.search) {
-        keywords = page.search.keywords || []
-      }
-
-      // Add to index
-      var doc = {
-        url: this.output.toURL(page.path),
-        title: page.title,
-        summary: page.description,
-        keywords: keywords.join(' '),
-        body: text
-      }
-
-      documentsStore[doc.url] = doc
-
-      return page
+    book: {
+        assets: './assets',
+        js: [
+            'jquery.mark.min.js',
+            'search.js'
+        ],
+        css: [
+            'search.css'
+        ]
     },
 
-    // Write index to disk
-    'finish': function () {
-      if (this.output.name !== 'website') return
+    hooks: {
+        // Index each page
+        'page': function (page) {
+            if (this.output.name !== 'website' || page.search === false) {
+                return page;
+            }
 
-      this.log.debug.ln('write search index')
-      return this.output.writeFile('search_plus_index.json', JSON.stringify(documentsStore))
+            this.log.debug.ln('index page', page.path);
+
+            console.log(page);
+
+            let text = page.content;
+
+            // strip HTML tags
+            text = text.replace(/<[^>]+>/g, '');
+
+            // shrink whitespaces to one space
+            text = text.replace(/[\n \x7f]+/g, ' ');
+
+            // unescape HTML
+            text = Html.decode(text);
+
+            this.log.debug.ln('stored content', text);
+
+            let keywords = []
+            if (page.search) {
+                keywords = page.search.keywords || [];
+            }
+
+            console.log("search text:" +  text);
+
+            // Add to index
+            const doc = {
+                url: this.output.toURL(page.path),
+                title: page.title,
+                summary: page.description,
+                keywords: keywords.join(' '),
+                body: text
+            };
+
+            documentsStore[doc.url] = doc;
+
+            return page;
+        },
+
+        // Write index to disk
+        'finish': function () {
+            if (this.output.name !== 'website')
+                return;
+
+            this.log.debug.ln('write search index');
+            return this.output.writeFile('search_plus_index.json', JSON.stringify(documentsStore));
+        }
     }
-  }
 }
